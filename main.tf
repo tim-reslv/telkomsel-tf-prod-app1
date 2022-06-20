4cbdf8aa744f7a363d589d6095a65222949ecd36
Tim Hsieh <tim@reslv.io>
2022-06-20 11:06:36 +0800

    rename eks

--- main.tf
+++ main.tf
@@ -2,37 +2,20 @@ provider "aws" {
   region = local.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-#  load_config_file       = false
-#  version                = "~> 1.12"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+  }
 }
-
-
-#provider "kubernetes" {
-#  host                   = module.eks.cluster_endpoint
-#  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#
-#  exec {
-#    api_version = "client.authentication.k8s.io/v1alpha1"
-#    command     = "aws"
-#    # This requires the awscli to be installed locally where Terraform is executed
-#    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-#  }
-#}
 
 locals {
-  name            = "prod-App-1"
+  name            = "prod-consul-client-eks"
   cluster_version = "1.22"
   region          = "ap-southeast-3"
 
